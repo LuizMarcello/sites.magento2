4849780d4fd98f88489206c3ff2f047c41714904
luizmarcello <luizmarcello@DESKTOP-C7614T3>
2024-06-14 11:41:37 -0300

    sampleData_TraduzidoPort

---
+++ vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'b9f3d4a1ae2c1685bca38d2f199c3eaa5f37c343',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'b9f3d4a1ae2c1685bca38d2f199c3eaa5f37c343',
+            'dev_requirement' => false,
+        ),
+        'rafaelstz/traducao_magento2_pt_br' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'magento2-language',
+            'install_path' => __DIR__ . '/../rafaelstz/traducao_magento2_pt_br',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'reference' => 'dac44535a1886dd6b25807fd0c1d7090de19032e',
+            'dev_requirement' => false,
+        ),
+    ),
+);
